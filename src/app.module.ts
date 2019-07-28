643c288f90762b88c8c176f8efe1bf79d3982d8c
haohaiwang <dudulu@haohaiwangdeMBP.lan>
2019-07-28 16:43:06 +0800

    feat:complete overview

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { CatsModule } from './cats/cats.module';
 import { AppService } from './app.service';
 import { CatsService } from './cats/cats.service';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { setUserinfoMiddleware } from './common/middlewares/set-userinfo.middleware';
 
 @Module({
   imports: [
@@ -15,4 +17,8 @@ import { CatsService } from './cats/cats.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware, setUserinfoMiddleware).forRoutes('/');
+  }
+}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,24 +1,56 @@
 // controllers are responsible for handling incoming requests and return responses to the client
-import { Controller, Get, Param, Req, Query, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Req,
+  Query,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  UseFilters,
+  ForbiddenException,
+  UsePipes,
+  UseGuards,
+  SetMetadata,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat';
+import { HttpExceptionFilter } from '../common/http_exception.filter';
+import { ValidationPipe } from './validation.pipe';
+import { RolesGuard } from '../common/roles.guard';
+import { Roles } from '../common/roles.decorator';
+import { LoggingInterceptor } from '../common/logging.interceptor';
+import { TransformInterceptor } from './transform.interceptor';
+import { User } from '../common/user.decorator';
 
 @Controller('cats')
+@UseInterceptors(LoggingInterceptor)
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
   @Get(':id')
+  @UseFilters(HttpExceptionFilter)
   findOne(@Param() params, @Req() req: Request, @Query() query): string {
-    console.log(req.path, query);
+    if (!/^\d+$/.test(params.id)) {
+      throw new ForbiddenException();
+    }
     return 'find ' + params.id;
   }
   @Get()
+  @UseInterceptors(TransformInterceptor)
   async finaAll(): Promise<Cat[]> {
     return this.catsService.findAll();
   }
   @Post()
-  async createOne(@Body() createcatDto: CreateCatDto) {
+  @Roles('admin')
+  @UsePipes(ValidationPipe)
+  async createOne(@Body() createcatDto: CreateCatDto, @User() user: any) {
+    console.log('user', user);
     this.catsService.create(createcatDto);
   }
 }
